2f4340a97b170d93babd9f69af7fa6b75643d030
AppleChien <82246767+AppleChien@users.noreply.github.com>
2022-01-28 16:34:09 +0800

    Add files via upload

---
+++ 0006.cpp
@@ -0,0 +1,33 @@
+static auto fast_io = []
+{
+	ios::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+	return 0;
+}();
+class Solution {
+public:
+    string convert(string s, int numRows)
+    {
+        if (numRows == 1) return s;
+        int n = s.length(), j = -1;
+        bool reverse = false;
+        string res;
+        vector<string> v(numRows);
+        for (auto& c : s)
+        {
+            if (!reverse)
+            {
+                v[++j] += c;
+                if (j >= numRows - 1) reverse = true;
+            }
+            else
+            {
+                v[--j] += c;
+                if (!j) reverse = false;
+            }
+        }
+        for (auto& str : v) res += str;
+        return res;
+    }
+};
