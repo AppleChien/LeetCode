617f844af4606197f67208a3914dbdaf466ac472
AppleChien <82246767+AppleChien@users.noreply.github.com>
2025-07-01 18:44:39 +0800

    Create 2226.cpp

---
+++ 2226.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+        int l=0,r=1e7;
+
+        while(l<r){
+            int mid=(l+r+1)>>1;
+
+            long long cnt=0;
+            for(int &x:candies)cnt+=x/mid;
+
+            if(cnt>=k)l=mid;
+            else r=mid-1;
+        }
+        return l;
+    }
+};
