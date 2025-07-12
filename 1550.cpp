93ddc2489672e4f6fca5201a8cd10cd6312e6d5f
AppleChien <82246767+AppleChien@users.noreply.github.com>
2025-07-12 15:35:37 +0800

    Create 1550.cpp

---
+++ 1550.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int cnt=0;
+        for(int a:arr){
+            if(a&1)++cnt;
+            else cnt=0;
+
+            if(cnt==3)return true;
+        }
+        return false;
+    }
+};
