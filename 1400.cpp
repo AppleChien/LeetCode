c05e41eb1f0098cd563599020c1f30bf5364a73c
AppleChien <82246767+AppleChien@users.noreply.github.com>
2025-07-18 01:35:33 +0800

    Create 1400.cpp

---
+++ 1400.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        if(s.size()<k)return false;
+        int cnt[26]{};
+        for(char &c:s)++cnt[c-'a'];
+
+        int x=0;
+        for(int v:cnt)x+=v&1;//幾個奇數
+
+        return x<=k;
+    }
+};
