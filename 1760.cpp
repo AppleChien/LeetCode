bdee194bfa189dcceb44b878f7796b39f374a264
AppleChien <82246767+AppleChien@users.noreply.github.com>
2025-07-23 20:59:21 +0800

    Create 1760.cpp

---
+++ 1760.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int left=1,right=*max_element(nums.begin(),nums.end());
+        while(left<right){
+            int mid=(left+right)>>1;
+            long long cnt=0;
+            for(int n:nums)cnt+=(n-1)/mid;
+
+            if(cnt<=maxOperations)right=mid;
+            else left=mid+1;
+        }
+
+        return left;
+    }
+};
