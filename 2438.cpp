2957f44dd7b4659c3c76f7ab8bc4d479e3793bef
AppleChien <82246767+AppleChien@users.noreply.github.com>
2025-08-11 11:31:43 +0800

    Create 2438.cpp

---
+++ 2438.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    const int mod=1e9+7;
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        vector<int>power;
+        while(n){
+            int x=n&-n;
+            power.emplace_back(x);
+            n-=x;
+        }
+
+        vector<int>ans;
+        for(auto &q:queries){
+            int l=q[0],r=q[1];
+            long long x=1l;
+            for(int j=l;j<=r;j++){
+                x=(x*power[j])%mod;
+            }
+
+            ans.emplace_back(x);
+        }
+
+        return ans;
+    }
+};
